aa08e315709897560454e77fd8ef8114d135d65f
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2022-10-02 18:42:56 +0530

    Added a pagination in tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -30,6 +30,18 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 5) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = Tour.countDocuments();
+      if (skip > numTours) throw new Error('This Page does not exist');
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
