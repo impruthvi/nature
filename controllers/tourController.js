a7363ab1acb21ed2f9622260707cfd6a6c7c4314
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2022-10-02 18:11:31 +0530

    Created a sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -12,7 +12,15 @@ exports.getAllTours = async (req, res) => {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|let|lt)\b/g, match => `$${match}`);
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 3) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     // EXECUTE QUERY
     const tours = await query;
