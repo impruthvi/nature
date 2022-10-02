c97ac0d9df42ec1d124e5c324c308a6aaa4cd36f
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2022-10-02 18:21:47 +0530

    Added a field limit in tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -22,6 +22,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // 4) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
