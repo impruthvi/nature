e7d3dc3ebb739ab1a77026a2ee42129c5f7502f1
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2022-10-03 21:58:21 +0530

    handled uncaught Exception

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
+process.on('uncaughtException', err => {
+  console.error('UNCAUGHT EXCEPTION! Shutting down..');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
