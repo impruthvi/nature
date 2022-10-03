b70a4912d4230f9c5ec6fb8fb8423c74384b8b6a
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2022-10-03 18:10:25 +0530

    differentiates the errors for production and development

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,39 @@
+const sendErrDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    err: err,
+    stack: err.stack
+  });
+};
+
+const sendErrProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+
+    // Programming or other error: don't leak error details
+  } else {
+    // 1) Log error message
+    console.error('Error 💣', err);
+
+    // 2) Send ganeric message
+    res.status(500).json({
+      status: 'error',
+      message: 'Somthing went very wrong'
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'fail';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env.NODE_ENV === 'devlopment') {
+    sendErrDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrProd(err, res);
+  }
 };
