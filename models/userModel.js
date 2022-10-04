af1279dff45f985953d8711dd287917314058ad6
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2022-10-04 13:55:51 +0530

    hash the password before the save

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 // name,email,photo,passowrd
 const userSchema = new mongoose.Schema({
@@ -22,8 +23,27 @@ const userSchema = new mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'Please confirm your passowrd']
+    required: [true, 'Please confirm your passowrd'],
+    validate: {
+      // ONLY WORK ON SAVE and CREATE
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: 'Password are not the same'
+    }
   }
+});
+
+userSchema.pre('save', async function(next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  //   Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //   Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('user', userSchema);
