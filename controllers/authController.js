4049c7967bd24f203057bfded671e85b626cf8dc
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2022-10-04 18:25:32 +0530

    Getting token and check of it's there

--- controllers/authController.js
+++ controllers/authController.js
@@ -48,3 +48,26 @@ exports.login = catchAsync(async (req, res, next) => {
     token
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check of it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer ')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged in ! Please log in to get access.', 401)
+    );
+  }
+  // 2) Verification token
+
+  // 3) Check it user still exists
+
+  // 4)Check if user change password after the token was issued
+  next();
+});
