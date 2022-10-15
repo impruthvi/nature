022e6cc66086a146d75df12cd87d705442f1fd45
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2022-10-15 19:08:16 +0530

    rename freshUser to currentUser in authController file

--- controllers/authController.js
+++ controllers/authController.js
@@ -71,18 +71,18 @@ exports.protect = catchAsync(async (req, res, next) => {
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
   // 3) Check it user still exists
-  const freshUser = await User.findById(decoded.id);
-  if (!freshUser) {
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
     return next('The user belonging to this token does no longer exist.', 401);
   }
   // 4)Check if user change password after the token was issued
-  if (freshUser.changedPasswordAfter(decoded.iat)) {
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
     return next(
       new AppError('User recently changed password! Please log in again', 401)
     );
   }
 
   // GRANT ACCESS TO PROTECTED ROUTES
-  req.user = freshUser;
+  req.user = currentUser;
   next();
 });
