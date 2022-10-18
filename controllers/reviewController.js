6099f24f42e213ece96f4dc90b53d6101109548a
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2022-10-18 11:11:27 +0530

    impliment nested route for reviews
    In this now when user add the reviews user id and tour id automatically added in review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -14,6 +14,9 @@ exports.getAllReview = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
+  // Allowed nested route
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
   const newReview = await Review.create(req.body);
 
   res.status(201).json({
