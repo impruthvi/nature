46c9e2d724054430a4fd788a77cb8de49ffbad98
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2022-11-16 15:07:22 +0530

    display the static tour card

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,6 +1,14 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+
+exports.getOverview = async (req, res) => {
+  // 1) Get Tour data from collection
+  const tours = await Tour.find();
+  // 2) Build template
+  // 3) Render that template using tour data from 1)
+
   res.status(200).render('overview', {
-    title: 'All tours'
+    title: 'All Tours',
+    tours
   });
 };
 
