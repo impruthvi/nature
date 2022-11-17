5c36cd863c6098712d2094566d02c43981be63f1
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2022-11-17 13:01:26 +0530

    created a login page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -27,3 +27,9 @@ exports.getTour = catchAsync(async (req, res) => {
     tour
   });
 });
+
+exports.getLoginForm = catchAsync(async (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account'
+  });
+});
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,7 +4,7 @@ const viewController = require('../controllers/viewController');
 const router = express.Router();
 
 router.get('/', viewController.getOverview);
-
 router.get('/tours/:slug', viewController.getTour);
+router.get('/login', viewController.getLoginForm);
 
 module.exports = router;
