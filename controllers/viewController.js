12fa5b243219f51cd48d791524ecc33c3d3a90c3
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2022-11-17 18:37:38 +0530

    display the error pages

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,5 +1,6 @@
 const catchAsync = require('../units/catchAsync');
 const Tour = require('../models/tourModel');
+const AppError = require('../units/appError');
 
 exports.getOverview = catchAsync(async (req, res) => {
   // 1) Get Tour data from collection
@@ -13,12 +14,16 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // 1) Get the data, for the requested tour(including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user'
   });
+
+  if (!tour) {
+    return next(new AppError('There is no tour with that name', 404));
+  }
 
   // 2) Build template
   // 3) Render that template using tour data from 1)
