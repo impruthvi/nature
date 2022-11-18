38821f460ac9262830f7899373cbb72e5af3b9eb
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2022-11-18 17:12:55 +0530

    send the reset password email

--- controllers/authController.js
+++ controllers/authController.js
@@ -173,17 +173,13 @@ exports.forgotPassword = async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   // 3) Send it to user's email
-  const resetURL = `${req.protocol}://${req.get(
-    'host'
-  )}/api/v1/users/resetPassword/${resetToken}`;
 
-  const message = `Forgot your password?Submit a PATCH request with your new password and passwordConfirm to:${resetURL}`;
   try {
-    // await sendEmail({
-    //   email: user.email,
-    //   subject: 'Your password reset Token (Valid for 10 min)',
-    //   message
-    // });
+    const resetURL = `${req.protocol}://${req.get(
+      'host'
+    )}/api/v1/users/resetPassword/${resetToken}`;
+    await new Email(user, resetURL).sendPasswordReset();
+
     res.status(200).json({
       status: 'success',
       message: 'Token send to email'
--- units/email.js
+++ units/email.js
@@ -49,4 +49,11 @@ module.exports = class Email {
   async sendWelcome() {
     await this.send('welcome', 'Welcome to the natours family');
   }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for only 10 min)'
+    );
+  }
 };
