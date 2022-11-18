33c16b8115ebe37a07101944257c811ea00f50fc
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2022-11-18 15:11:26 +0530

    upload the tour images and cover image

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,62 @@
+const multer = require('multer');
+const sharp = require('sharp');
 const Tour = require('../models/tourModel');
 const catchAsync = require('../units/catchAsync');
 const factory = require('./factoryHandler');
 const AppError = require('../units/appError');
 // const APIFeatures = require('../units/apiFeaturs');
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images.', 404));
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter
+});
+
+exports.uploadTourImages = upload.fields([
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 3 }
+]);
+exports.resizeTourImages = async (req, res, next) => {
+  if (!req.files.imageCover || !req.files.images) return next();
+
+  // 1)Cover image
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jped`;
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+  // Images
+  req.body.images = [];
+
+  await Promise.all(
+    req.files.images.map(async (file, i) => {
+      const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jped`;
+
+      await sharp(file.buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${filename}`);
+
+      req.body.images.push(filename);
+    })
+  );
+
+  next();
+};
+
+// upload.single('imageCover');
+// upload.array('images', 5);
 
 exports.aliasTopTours = async (req, res, next) => {
   req.query.limit = 5;
