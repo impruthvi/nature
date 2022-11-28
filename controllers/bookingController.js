a81f6e72af697dd732a69c6497a62f3a4f0527ee
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2022-11-28 18:51:51 +0530

    Update bookingController.js

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -62,7 +62,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookinfCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.find({ email: session.customer_email })).id;
-  const price = session.line_items[0].price_data.unit_amount / 100;
+  const price = session.amount_total / 100;
   console.log(`Booking`, tour, user, price);
   await Booking.create({ tour, user, price });
 };
@@ -82,7 +82,6 @@ exports.webhookCheckout = catchAsync(async (req, res, next) => {
   }
 
   if (event.type === 'checkout.session.completed') {
-    conso;
     createBookinfCheckout(event.data.object);
   }
 
