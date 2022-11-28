727e8c430ed3be3fceff2d1b9f6d8acc492e882e
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2022-11-28 15:27:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ const server = app.listen(port, () => {
 
 process.on('unhandledRejection', err => {
   console.log(err.name, err.message);
+  console.log(err);
   console.log('UNHANDLED REJECTION! 🧨 Shutting down');
   server.close(() => {
     process.exit(1);
