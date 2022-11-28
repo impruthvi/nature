db8c8ebae212920c10310017f644f5ba078b811e
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2022-11-28 17:08:10 +0530

    Configure the mongoose

--- server.js
+++ server.js
@@ -2,13 +2,6 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
-mongoose.connect(
-  'mongodb+srv://test:test1234@todolist.wd9gx.mongodb.net/natours?retryWrites=true&w=majority/natours',
-  {
-    useNewUrlParser: true
-  }
-);
-
 process.on('uncaughtException', err => {
   console.error('UNCAUGHT EXCEPTION! Shutting down..');
   console.log(err.name, err.message);
@@ -17,12 +10,19 @@ process.on('uncaughtException', err => {
 
 dotenv.config({ path: './config.env' });
 
-// const DB = process.env.DATABASE.replace(
-//   '<PASSWORD>',
-//   process.env.DATABASE_PASSWORD
-// );
-// const DB =
-//   'mongodb://impruthvi:impruthvi@todolist-shard-00-00.wd9gx.mongodb.net:27017,todolist-shard-00-01.wd9gx.mongodb.net:27017,todolist-shard-00-02.wd9gx.mongodb.net:27017/natours?ssl=true&replicaSet=atlas-a3xgj5-shard-0&authSource=admin&retryWrites=true&w=majority';
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Database connection successfull'));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
