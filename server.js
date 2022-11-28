30977b57a3b9db42a8a4edbd0762bc9d6cf15ab5
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2022-11-28 15:40:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,21 +10,19 @@ process.on('uncaughtException', err => {
 
 dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
-
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
+// );
+const DB =
+  'mongodb://impruthvi:impruthvi@todolist-shard-00-00.wd9gx.mongodb.net:27017,todolist-shard-00-01.wd9gx.mongodb.net:27017,todolist-shard-00-02.wd9gx.mongodb.net:27017/natours?ssl=true&replicaSet=atlas-a3xgj5-shard-0&authSource=admin&retryWrites=true&w=majority';
 mongoose
-  .connect(
-    'mongodb+srv://impruthvi:impruthvi@todolist.wd9gx.mongodb.net/natours?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      // useFindAndModify: false,
-      useUnifiedTopology: true
-    }
-  )
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    // useFindAndModify: false,
+    useUnifiedTopology: true
+  })
   .then(() => console.log('Database connection successfull'));
 
 const port = process.env.PORT || 3000;
