91289f2b4af0eb5b8857aa3d09bd0f7ea03766f1
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2022-11-28 17:49:51 +0530

    implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./units/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -25,6 +26,8 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+app.use(cors());
+app.options('*', cors());
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
