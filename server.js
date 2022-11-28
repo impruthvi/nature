a8ff7a1677814b2632222f752bbc8aefcfeef468
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2022-11-28 14:46:41 +0530

    chnages

--- server.js
+++ server.js
@@ -10,18 +10,21 @@ process.on('uncaughtException', err => {
 
 dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
-console.log(DB);
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
+// );
+
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  })
+  .connect(
+    'mongodb+srv://impruthvi:impruthvi@todolist.wd9gx.mongodb.net/natours?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      // useCreateIndex: true,
+      // useFindAndModify: false,
+      useUnifiedTopology: true
+    }
+  )
   .then(() => console.log('Database connection successfull'));
 
 const port = process.env.PORT || 3000;
