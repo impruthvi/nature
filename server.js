d459608903189025d53ebc30638401dd8c3bba28
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2022-11-28 14:18:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -38,11 +38,11 @@ const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
-process.on('unhandledRejection', err => {
-  console.log(err.name, err.message);
-  // console.log(err);
-  console.log('UNHANDLED REJECTION! 🧨 Shutting down');
-  server.close(() => {
-    process.exit(1);
-  });
-});
+// process.on('unhandledRejection', err => {
+//   console.log(err.name, err.message);
+//   // console.log(err);
+//   console.log('UNHANDLED REJECTION! 🧨 Shutting down');
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
